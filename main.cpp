e13137c1538e831d4388ff6ad2aa760537a3a2d4
lyj-Desktop <liu.yunjie@qq.com>
2023-08-25 16:50:59 +0800

    2nd commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.resize(320, 240);
+    w.show();
+    //w.setWindowTitle(
+    //    QApplication::translate("toplevel", "Top-level widget"));
+    // QT_VERSION_MAJOR in the qconfig.h
+    w.setWindowTitle(QString::number(QT_VERSION_MAJOR)+ "-" + QT_VERSION_STR);
+    return a.exec();
+}
